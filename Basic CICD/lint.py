2d92a34d1154352a75d3b5f0ddf85ff145c2c9b0
2dogz <kylewebb58@yahoo.com>
2022-02-03 16:46:39 -0600

    update lint.py

--- Basic CICD/lint.py
+++ Basic CICD/lint.py
@@ -9,11 +9,10 @@ THRESHOLD = 9
 run = lint.Run(["factorial.py"])
 
 score = run.linter.stats["global_note"]
-print(score)
+
 if score < THRESHOLD:
 
     print("Linter failed: Score < threshold value")
 
 
 else:
-    print(score)
