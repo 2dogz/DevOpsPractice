839a287b1ab93aff99fc6eceefc41de52619e5be
2dogz <kylewebb58@yahoo.com>
2022-02-03 20:11:45 -0600

    change threshold for test

--- Basic CICD/lint.py
+++ Basic CICD/lint.py
@@ -4,12 +4,11 @@ import sys
 
 from pylint import lint
 
-THRESHOLD = 9
+THRESHOLD = 8
 
 run = lint.Run(["factorial.py"])
 
 score = run.linter.stats["global_note"]
 
 if score < THRESHOLD:
-
     print("Linter failed: Score < threshold value")
